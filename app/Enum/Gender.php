10cfe65cf05fad2e4c04ed795ff15c91451221a6
VosMovok <dimasnugraha46@gmail.com>
2025-04-29 13:37:21 +0800

    Revert "init"
    This reverts commit f01fe7b85eee2a03bd3071810c7b71a684df80b3.

--- app/Enum/Gender.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enum;
-
-enum Gender: String
-{
-    case MALE  = "Male";
-    case FEMALE = "Female";
-}
--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\UserDetailController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::apiResource('users/detail', UserDetailController::class);
